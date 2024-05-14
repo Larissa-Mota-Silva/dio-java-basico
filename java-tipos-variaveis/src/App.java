4cba250b9850dda4355ec7925f4524c2a85cc7cc
Larissa Mota <contato.larissamotasilva@gmail.com>
2024-05-13 22:56:27 -0300

    commit operadores

--- java-tipos-variaveis/src/App.java
+++ java-tipos-variaveis/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Testando os tipos 
+        double salarioMinino = 2500;
+        int numero = 5;
+        String nome = "Carla";
+        boolean verdadeiro = true;
+        char sexo = 'M'; //Só permite um unico caractere
+        String resul;
+        
+        final double VALORPI = 3.14; //O final define a variável como constante
+    
+        //Testando os operadores
+        System.out.println(- numero); //Operador binário que transforma em negativo
+    
     }
 }
