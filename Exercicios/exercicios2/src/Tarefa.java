f86462981cb8af026571e9ff80e670f3ad73b774
Larissa Mota <contato.larissamotasilva@gmail.com>
2024-07-18 17:23:01 -0300

    resolvendo exercicio

---
+++ Exercicios/exercicios2/src/Tarefa.java
@@ -0,0 +1,12 @@
+public class Tarefa {
+    //Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
